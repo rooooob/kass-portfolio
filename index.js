64dc3456931eb3849e2a1af27b3b7a556df8e1ee
= <=>
2021-10-27 20:42:55 -0400

    t2

---
+++ index.js
@@ -0,0 +1,7 @@
+import { App } from "./src/App.js";
+import { Animations } from "./src/helpers/animations.js";
+
+document.addEventListener("DOMContentLoaded", e => {
+    App();
+    Animations();
+})
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import { App } from "./App.js";
-import { Animations } from "./helpers/animations.js";
-
-document.addEventListener("DOMContentLoaded", e => {
-    App();
-    Animations();
-})
